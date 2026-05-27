a505227221b170ffb28e7158c958bf13e7b3d66d
Trinidad Bonzi <trinidad.bonzi.ices@gmail.com>
2026-05-27 10:43:01 -0300

    actionslisto

--- entrega1.py
+++ entrega1.py
@@ -29,8 +29,50 @@ class RoverProblem (SearchProblem):
         
         acciones = []
         
-        if bateriaInicial > 0:
-            acciones.append("moverse")
+        x, y = posicion
+        
+        movimientos = [
+            (x - 1, y),
+            (x + 1, y);
+            (x, y - 1);
+            (x, y + 1);
+        ]        
+        
+        if bateriaInicial > 1:
+            for destino in movimientos:
+                acciones.append(("moverse", destino))
+                
+        sobremarchas = [
+            (x - 2, y),
+            (x + 2, y),
+            (x, y - 2),
+            (x, y + 2),
+        ]
+        
+        if bateriaInicial > 4:
+            for destino in sobremarchas:
+                acciones.append(("sobremarcha", destino))
+         
+        if bateriaInicial > 1:
+            if taladro != "termico":
+                acciones.append("equipar", "termico")
             
+            if taladro != "percusion":
+                acciones.append(("equipar", "percusion")) 
+                
+        if carga > self.capacidad_maxima and bateriaInicial > 3:
+            if posicionRover in mIgnea and taladro == "termico":
+                acciones.append(("recolectar", "ignea"))
+                
+            if posicionRover in mSedimentaria and taladro == "percusion":
+                acciones.append(("recolectar", "sedimentaria"))
+                
+        if carga > 0 and bateriaInicial > 1:
+            acciones.append(("depositar", None)) 
+            
+        if posicionRover not in self.zonas_sombra and bateriaInicial < self.bateria_maxima>:
+            acciones.append(("recargar", None))
         
         return acciones
+    
+    
