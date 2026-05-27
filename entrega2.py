d9773217e7dcf7d2a63298681b2b3725cefadbbb
Trinidad Bonzi <trinidad.bonzi.ices@gmail.com>
2026-05-27 16:27:13 -0300

    Avance

--- entrega2.py
+++ entrega2.py
@@ -4,5 +4,19 @@ def build_camp(camp_size, habs, generators, labs, deposits, airlocks, craters):
     fila, columna = camp_size
     
     variables = []
+    
+    for i in range(habs):
+        variables.append(("hab", i))
+    for i in range(generators):
+        variables.append(("gen", i))   
+    for i in range(labs):
+        variables.append(("lab", i)) 
+    for i in range(deposits):
+        variables.append(("dep", i))
+    for i in range(airlocks):
+        variables.append(("air", i))   
     dominios = {}
     restricciones = []
+    
+    
+    
